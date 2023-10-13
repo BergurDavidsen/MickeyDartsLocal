6a653ac276a34aef69437090774c5c7d32ed3d11
BergurDavidsen <bergurdavidsen@gmail.com>
2023-10-13 23:17:50 +0200

    update: Moved button to top page

--- src/routes/stats/previousgames/+page.svelte
+++ src/routes/stats/previousgames/+page.svelte
@@ -82,6 +82,11 @@
 	<h1 class="my-4 font-bold text-5xl text-yellow-500 border-2 border-red-500 p-5 rounded-xl">
 		Game History
 	</h1>
+	<button
+		on:click={() => goto('/stats')}
+		class="my-4 text-yellow-700 hover:text-white border border-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-500 dark:text-yellow-500 dark:hover:text-white dark:hover:bg-yellow-500 dark:focus:ring-yellow-800"
+		>Back To Stats</button
+	>
 	{#if Object.keys(games).length < 1}
 		<div class="flex flex-col justify-center items-center text-center my-20">
 			<h1 class="text-red-500 text-4xl font-bold">No Games Played Yet</h1>
@@ -105,9 +110,4 @@
 			<BubbleChart mydata={dataset} myID={Object.keys(games)[index]} />
 		{/each}
 	{/if}
-	<button
-		on:click={() => goto('/stats')}
-		class="my-4 text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-500 dark:focus:ring-green-800"
-		>Back To Stats</button
-	>
 </div>
