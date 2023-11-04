cff075c8846ff50cc1fea01d9cc9045cdaa9bbbe
BergurDavidsen <bergurdavidsen@gmail.com>
2023-11-04 17:54:20 +0100

    new clean database

--- src/routes/stats/+page.server.js
+++ src/routes/stats/+page.server.js
@@ -3,11 +3,12 @@ import {db} from "$lib/db.js";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-const players = await db.mickey_real_data_v_4.findMany({
+const players = await db.mickey_real_v_6.findMany({
     
-    
+   
 });
-const winners = await db.mickey_real_data_v_4.findMany({
+
+const winners = await db.mickey_real_v_6.findMany({
     
     where: {
       iswinner: "true",
