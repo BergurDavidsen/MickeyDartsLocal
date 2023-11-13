979c76cb50b0147bea087b51c7d97ffbc4311307
BergurDavidsen <bergurdavidsen@gmail.com>
2023-11-13 17:01:50 +0100

    fix: wrong text in buttons

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -172,7 +172,7 @@
 			showAllTime = !showAllTime;
 		}}
 		class="mb-7 text-yellow-700 hover:text-white border border-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:border-yellow-500 dark:text-yellow-500 dark:hover:text-white dark:hover:bg-yellow-500 dark:focus:ring-yellow-800"
-		>See Stats For Everyone</button
+		>{showAllTime ? 'See Your Stats' : 'See Stats For Everyone'}</button
 	>
 
 	<a
@@ -214,7 +214,7 @@
 				showAllWinners = !showAllWinners;
 			}}
 			class="mb-7 text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-500 dark:focus:ring-green-800"
-			>Show All Winners</button
+			>{showAllWinners ? 'Show Less Winners' : 'Show All Winners'}</button
 		>
 
 		<table class="table-auto w-full bg-white rounded-xl text-center my-4">
